08fde306f38bedaa05f8a94d6161d531c7d43ee0
Josh Hassara <josh.hassara@joonbug.com>
2011-09-27 18:05:08 -0400

    blog tweaks

--- pages/_blog.slug_/_blog.slug_.php
+++ pages/_blog.slug_/_blog.slug_.php
@@ -11,7 +11,6 @@ $clause['blog_id'] = $blog_id;
 $blog_articles = blog_article::getList($clause);
 $grid = new array_pagination_qf($blog_articles);
 if ($grid->rs) {
-
 	foreach ( $grid->rs as $blog_article_id) {
 		$r = new blog_article($blog_article_id);
 		include("pages/_blog.slug_/mini.php");
@@ -23,7 +22,9 @@ if ($grid->rs) {
 	?>
     </div>
 	<?
-}
+} else { ?>
+	There are no blog articles on this website.
+<? } 
 
 $p->template('website', 'bottom');	
 ?>
--- pages/blog/blog.php
+++ pages/blog/blog.php
@@ -5,6 +5,10 @@ if($json->blog_id)
 if($json->blog_tag)	
 	$blog_where[] = "(blog_article_tag.name ILIKE '".implode("' OR blog_article_tag.name ILIKE '",$json->blog_tag)."')";
 
+// IF NO $blog_where, set $blog_where to flase so no blogs show up.
+if(!$blog_where)
+	$blog_where[] = 'false';
+
 $clause['where'] = "( ".implode(' AND ',$blog_where)." )" ;
 
 $website['blog_json'] = $website;
