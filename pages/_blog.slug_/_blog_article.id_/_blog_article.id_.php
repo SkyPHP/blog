e4e12334abf51eada5ef3d0ea85b871ce4d3bffd
Josh Hassara <josh.hassara@joonbug.com>
2012-02-21 14:04:19 -0500

    redirect is blog id and blog slug do not match. go to blog slug if they are the same blog. go to /blog if they are different blogs

--- pages/_blog.slug_/_blog_article.id_/_blog_article.id_.php
+++ pages/_blog.slug_/_blog_article.id_/_blog_article.id_.php
@@ -3,6 +3,15 @@ $sidebar = 'ad';
 
 $r= new blog_article($blog_article_id);
 
+if ($p->queryfolders[0] != slugize($r->title)){  
+	if(IDE == $r->blog_slug)
+		redirect('/'.$r->blog_slug);
+	else
+		redirect('/blog');
+}
+
+
+
 $p->js[]='http://maps.google.com/maps/api/js?sensor=false&language=en';
     
 
@@ -11,7 +20,6 @@ $p->breadcrumb = array( 'Home' => '/',
 						$r->title => NULL);
 
 $p->template('website', 'top');	
-
 include("article.php");
 
 $p->template('website', 'bottom');	
